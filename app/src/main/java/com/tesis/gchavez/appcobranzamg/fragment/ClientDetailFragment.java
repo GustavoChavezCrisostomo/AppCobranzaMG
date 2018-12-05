cffefabdb17d5d5588a438249606db32101f49b4
Gustavo Chavez <gustavo.chavez.c@tecsup.edu.pe>
2018-12-05 14:34:07 -0500

    avance client

--- app/src/main/java/com/tesis/gchavez/appcobranzamg/fragment/ClientDetailFragment.java
+++ app/src/main/java/com/tesis/gchavez/appcobranzamg/fragment/ClientDetailFragment.java
@@ -1,17 +1,22 @@
 package com.tesis.gchavez.appcobranzamg.fragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import com.tesis.gchavez.appcobranzamg.R;
 
 
-public class ClientDetailFragment extends Fragment {
+
+public class ClientDetailFragment extends DialogFragment implements View.OnClickListener {
 
 
     public ClientDetailFragment() {
@@ -27,12 +32,39 @@ public class ClientDetailFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
+    EditText date;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_client_detail, container, false);
+        View view =  inflater.inflate(R.layout.fragment_client_detail, container, false);
+
+        date = (EditText) view.findViewById(R.id.date_input);
+        date.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.date_input:
+                showDatePickerDialog();
+                break;
+        }
+    }
+
+    private void showDatePickerDialog() {
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                // +1 because january is zero
+                final String selectedDate = day + " / " + (month+1) + " / " + year;
+                date.setText(selectedDate);
+            }
+        });
+        newFragment.show(getActivity().getSupportFragmentManager(), "datePicker");
     }
 
 }
+
