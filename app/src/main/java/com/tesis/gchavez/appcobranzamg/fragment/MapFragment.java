9d620f579e9d47006babab9a3e34566ad875d692
Gustavo Chavez <gustavo.chavez.c@tecsup.edu.pe>
2018-12-13 14:43:11 -0500

    avance map

--- app/src/main/java/com/tesis/gchavez/appcobranzamg/fragment/MapFragment.java
+++ app/src/main/java/com/tesis/gchavez/appcobranzamg/fragment/MapFragment.java
@@ -1,16 +1,18 @@
 package com.tesis.gchavez.appcobranzamg.fragment;
 
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.tesis.gchavez.appcobranzamg.R;
 
 import androidx.fragment.app.DialogFragment;
 
-public class MapFragment extends DialogFragment {
+public class MapFragment extends DialogFragment implements OnMapReadyCallback {
 
     public MapFragment() {
         // Required empty public constructor
@@ -32,10 +34,16 @@ public class MapFragment extends DialogFragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_map, container, false);
 
+        SupportMapFragment fragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        fragment.getMapAsync(this);
+
         return view;
     }
 
 
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
 
+    }
 }
